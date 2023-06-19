620c828f670c542320bd8bcd72d24e7138711049
erikchiodo <erikchiodo@gmail.com>
2023-06-19 12:17:43 -0400

    Updated Webpack.config with babel/css loader/inject manifest

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,9 +3,13 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {});
+// event for installion of an app -- show the bar or not
+
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {});
+// When they click the install button (install app)
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {});
+//After app is install, we want to hide (not show) bar
