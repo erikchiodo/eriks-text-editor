c6773f1d0c23b66f11ed776c3ddb81e4ee87b450
erikchiodo <erikchiodo@gmail.com>
2023-06-20 20:30:40 -0400

    Added event listeners to install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,15 +1,23 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
-// event for installion of an app -- show the bar or not
+window.addEventListener("beforeinstallprompt", (event) => {
+  window.deferredPrompt = event;
 
+  butInstall.classList.toggle("hidden", false);
+});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
-// When they click the install button (install app)
+butInstall.addEventListener("click", async () => {
+  const promptEvent = window.deferredPrompt;
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
-//After app is install, we want to hide (not show) bar
+  if (!promptEvent) {
+    return;
+  }
+  promptEvent.prompt();
+  window.deferredPrompt = null;
+  butInstall.classList.toggle("hidden", true);
+});
+
+window.addEventListener("appinstalled", (event) => {
+  window.deferredPrompt = null;
+}); 
